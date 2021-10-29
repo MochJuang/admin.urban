4f2e51cb876c4b6b7c6712bf7cdd8eceafee7fb5
MochJuang <mochjuangpp@gmail.com>
2021-10-29 10:44:04 +0700

    initial 1

--- application/helpers/service_helper.php
+++ application/helpers/service_helper.php
@@ -16,7 +16,7 @@ if (!function_exists('getservice')) {
             'Authorization'     => 'Bearer ' . $ci->session->userdata('access_token'),
         ];
         $client = new GuzzleHttp\Client([
-            'base_uri' => 'http://localhost:3000/v1/',
+            'base_uri' => 'https://backend.urbanco.co.id/v1/',
             'verify'          => false,
 
         ]);
@@ -54,7 +54,7 @@ if (!function_exists('login_service')) {
     {
         $ci = &get_instance();
         $client = new GuzzleHttp\Client([
-            'base_uri' => 'http://localhost:3000/v1/',
+            'base_uri' => 'https://backend.urbanco.co.id/v1/',
             'curl' => array(CURLOPT_SSL_VERIFYPEER => false, CURLOPT_SSL_VERIFYHOST => false),
             'allow_redirects' => false,
             'cookies'         => true,
@@ -94,7 +94,7 @@ if (!function_exists('check_token')) {
     function check_token()
     {
         $ci = &get_instance();
-        $client = new GuzzleHttp\Client(['base_uri' => 'http://localhost:3000/v1/']);
+        $client = new GuzzleHttp\Client(['base_uri' => 'https://backend.urbanco.co.id/v1/']);
         $request = $client->request('GET', 'auth/check_token', [
             'headers' => [
                 'Authorization'     => 'Bearer ' . $ci->session->userdata('access_token'),
