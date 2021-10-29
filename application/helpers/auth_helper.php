bc104a32719d16942a5b6fde184576094a4bbc19
MochJuang <mochjuangpp@gmail.com>
2021-10-29 15:01:04 +0700

    initial 2

--- application/helpers/auth_helper.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-if (!function_exists('is_login')) {
-    function is_login($data)
-    {
-        $ci = &get_instance();
-        $userdata = $ci->session->userdata($data);
-        return $userdata;
-    }
-}
